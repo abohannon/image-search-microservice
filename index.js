8e74fb3987d243a1ba1e67af4cab4f33f00263a4
Adam Bohannon <abo46n2@gmail.com>
2018-01-30 15:41:27 -0800

    Added recent searches route and mongoDB handling

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const routes = require('./routes')
-const PORT = process.env.PORT || 3000
+const mongoose = require('mongoose')
+require('./models/Search')
+const keys = require('./config/keys')
+
+mongoose.Promise = global.Promise
+mongoose.connect(keys.MONGO_URI)
 
 app.use(bodyParser.json())
 app.use(express.static(`${__dirname}/public`))
 
+const routes = require('./routes')
 routes(app)
 
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
 })
+
+module.exports = app
--- routes.js
+++ routes.js
@@ -1,6 +1,7 @@
 const fetch = require('node-fetch')
-const API_KEY = 'AIzaSyC0e615PFI4bXO7Fpe0ycu7h6Dr3nmWsCg'
-const SEARCH_ID = '007292438043507674194:yeh92fuem_y'
+const keys = require('./config/keys')
+const mongoose = require('mongoose')
+const Search = mongoose.model('search')
 
 module.exports = (app) => {
   app.get('/', (req, res) => res.send('Hello world'))
@@ -10,25 +11,46 @@ module.exports = (app) => {
     const { offset } = req.query
     const page = offset > 0 ? 10 * offset : 1
 
+    const newSearch = new Search({
+      query: term,
+      date: Date.now()
+    })
+
     try {
-      const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${SEARCH_ID}&searchType=image&start=${page}&q=${req.params.term}`)
+      const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${keys.API_KEY}&cx=${keys.SEARCH_ID}&searchType=image&start=${page}&q=${term}`)
       const body = await response.json()
 
-      const searchResults = body.items.reduce((results, item, i) => {
+      const searchResults = await body.items.reduce((results, item, i) => {
         results[i] = {
           title: item.title,
           'image url': item.link,
            context: item.image.contextLink
         }
-
         return results
       }, [])
+
+      await newSearch.save()
 
       res.send(searchResults)
 
     } catch (err) {
       console.log('There was an error fetching data', err)
     }
+  })
+
+  app.get('/api/recent', (req, res) => {
+
+    Search.find({}, (err, searches) => {
+      const recentSearches = searches.reduce((searches, search, i) => {
+        searches[i] = {
+          query: search.query,
+          date: search.date
+        }
+        return searches
+      }, [])
+
+      res.send(recentSearches.reverse())
+    })
   })
 
 }
